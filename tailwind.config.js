2601319e799fd0b040e0aa3390c75bc84aea2c98
OctavioPF <111895528+OctavioPF@users.noreply.github.com>
2024-01-07 11:34:11 -0800

    terminado

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
